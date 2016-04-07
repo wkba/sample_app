38622dc4448727772410abc1196d9575d3008bba
Wakabayashi syunsuke <wkba61@gmail.com>
2016-04-07 18:54:20 +0900

    tset

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,57 +4,57 @@ describe "StaticPages" do
   describe "GET /home" do
     it "should have the content 'Home'" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_content('Sample App')
     end
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit home_path
       expect(page).not_to have_title(" | Home")
     end
   end
   describe "help page" do
   	it "should have the content 'Help'" do
-  	  visit '/static_pages/help'
+  	  visit help_path
   	  expect(page).to have_content('Help')
   	end
   	it "should have the right title" do
-  	  visit '/static_pages/help'
+  	  visit help_path
   	  expect(page).to have_title("Ruby on Rails Tutorial Sample App")
   	end
   	it "should have not a custom page title" do
-  	  visit '/static_pages/help'
+  	  visit help_path
   	  expect(page).not_to have_title("|help")
   	end
   end
   describe "About page" do
   	it "should have the content 'About'" do
-  	  visit '/static_pages/about'
+  	  visit about_path
   	  expect(page).to have_content('About')
   	end
   	it "should have the right title"  do
-  	  visit '/static_pages/about'
+  	  visit about_path
   	  expect(page).to have_title("Ruby on Rails Tutorial Sample App")
   	end
   	it "should not have a coustom title" do
-  	  visit '/static_pages/about'
+  	  visit about_path
   	  expect(page).not_to have_title("|About")
   	end 
   end
-  describe "About contact page" do
+  describe "contact page" do
     it "should have the content 'contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the right title"  do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
     it "should not have a coustom title" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).not_to have_title("|Contact")
     end 
   end
